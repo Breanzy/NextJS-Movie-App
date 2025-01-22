c68368f677c6c296f2ed86802f695639fac2444b
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-01-22 23:47:10 +1100

    add Navbar component and TopPages route

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import ThemeComp from "@/components/ThemeComp";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
                 <body>
                     <ThemeComp>
                         <Header />
+                        <Navbar />
                         {children}
                     </ThemeComp>
                 </body>
