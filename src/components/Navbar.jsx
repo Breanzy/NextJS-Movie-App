1b20b9c1d3067be8ff17b18edad01762f3e7bcc5
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-01-26 23:57:22 +1100

    Sus commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar() {
         <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
             <NavbarItem title="Trending" param="trending" />
             <NavbarItem title="Top Rated" param="rated" />
+            I just want green heheheh
         </div>
     );
 }
