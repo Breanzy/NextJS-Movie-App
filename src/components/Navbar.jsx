ad191877945ef12f47677dad1633318ccf2dc038
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-01-30 09:48:26 +1100

    Fetch and display  movies and add results and Card components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,6 @@ export default function Navbar() {
         <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
             <NavbarItem title="Trending" param="trending" />
             <NavbarItem title="Top Rated" param="rated" />
-            Oh god I hope nobody sees this and think this is a reflection of
-            what I do for the rest of my programming commits. I swear this is
-            the only time that I'm doing these
         </div>
     );
 }
